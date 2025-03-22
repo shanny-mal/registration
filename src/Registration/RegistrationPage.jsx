871069e8de9b65e209dfd57779a83cdb652eb846
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-03-22 14:54:15 +0200

    correct path for footer

--- src/Registration/RegistrationPage.jsx
+++ src/Registration/RegistrationPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 import RegistrationCard from "./RegistrationCard";
-import Footer from "../Footer/Footer";
+import Footer from "../footer/Footer";
 import "../App.css";
 
 const RegistrationPage = () => {
